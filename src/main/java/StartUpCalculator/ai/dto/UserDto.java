249891d77ebedd0303e7f7884d9daa4a3981b9b7
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-04-08 13:09:32 +0900

    add dto

---
+++ src/main/java/StartUpCalculator/ai/dto/UserDto.java
@@ -0,0 +1,32 @@
+Package StartUpCalculator.ai.dto; 
+
+public class UserDto{
+  private int id;
+  private String name;
+  private String password;
+
+    public void setId(int id){
+      this.id=id;
+    }
+
+    public int getId(){
+      return id;
+    }
+    
+    public void setName(String name){
+      this.name=name;
+    } 
+
+    public String getName(){
+      return name;
+    }
+
+    public void setPassword(String password){
+      this.password= password;
+    }
+
+    public String getPassword(){
+      return password;
+    }
+}
+
