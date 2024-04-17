d4f769ee1c5adc57b613011daab44170ced89811
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-04-17 15:50:57 +0900

    clear small typos error

--- src/main/java/StartUpCalculator/ai/repository/UserRepository.java
+++ src/main/java/StartUpCalculator/ai/repository/UserRepository.java
@@ -1,14 +1,16 @@
 package StartUpCalculator.ai.repository;
 import StartUpCalculator.ai.model.User;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User,Long>  {
   List<User> findAll();
   
-  save();
+  void save();
 
-  @Query("select 1 from users where email= ?1)
+  @Query("select 1 from users where email= ?1")
   boolean existsByEmail(String email);
 
 }
