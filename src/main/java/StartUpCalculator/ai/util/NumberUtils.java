daee5ea0611dc016fa7b03366381efedefef697c
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-04-23 13:07:36 +0900

    add number utils

--- src/main/java/StartUpCalculator/ai/util/NumberUtils.java
+++ src/main/java/StartUpCalculator/ai/util/NumberUtils.java
@@ -4,4 +4,7 @@ public class NumberUtils{
   public boolean isNull(int number){
     return number== 0 ? true : false; 
   }
+  public boolean isNotNull(int number){
+    return number !=0 ? false : true;
+  }
 }
